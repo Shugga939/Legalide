3492fc67c447e4450475c7bc5f05c0451170e057
Shugga939 <Shugga939@gmail.com>
2022-07-02 18:03:32 +0300

    fix img's path

--- webpack.config.js
+++ webpack.config.js
@@ -58,8 +58,8 @@ const plugins = () => {
         new CleanWebpackPlugin(),
         new CopyWebpackPlugin([
             {
-                from: path.resolve(__dirname, 'src/favicon.ico'),
-                to: path.resolve(__dirname, 'dist')
+                from: path.resolve(__dirname, 'src/assets'),
+                to: path.resolve(__dirname, 'dist/src/assets')
             }
         ]),
         new MiniCssExtractPlugin({
